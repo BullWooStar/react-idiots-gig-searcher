2062ba3d47b25ed59783bf9b7fec48d7831ec79b
BullWooStar <ccatal66@naver.com>
2022-07-20 22:34:48 +0900

    feat: project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Input from "./components/Input";
+import GigList from "./components/GigList";
 
 function App() {
+  const [gigInfo, setGigInfo] = useState([]);
+
+  const searchGig = async (props) => {
+    const response = await fetch(
+      `https://api.idiots.band/api/search?keyword=${props}`
+    );
+    const result = await response.json();
+
+    setGigInfo(result);
+  };
+
+  let content = (
+    <p style={{ textAlign: "center" }}>
+      이디어츠는 누구와 같이 어떠한 공연을 했을까요?
+      <br />
+      밴드 이름을 검색해주세요~
+    </p>
+  );
+
+  if (gigInfo.length > 0) {
+    content = <GigList gigInfo={gigInfo} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input onAddValue={searchGig} />
+      <section> {content} </section>
     </div>
   );
 }
